3b88b0af4f6185b576c0854b2618050fa68eea60
eddyboon <66462284+eddyboon@users.noreply.github.com>
2023-08-27 23:19:43 +1000

    Created the login page

---
+++ src/pages/loginPage.js
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
+
+// use navigate("/Dashboard") to go redirect user to dashboard after user auth
+
+const LoginPage = () => {
+  return (
+    <div>
+      <H1>Login Here!</H1>
+    </div>
+  );
+};
