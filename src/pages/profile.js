d2ffb6a4b75d0dbabea475f123111691b09e774b
eddyboon <66462284+eddyboon@users.noreply.github.com>
2023-09-03 00:14:45 +1000

    Added update particulars field

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,81 @@
-import React from 'react';
+import { ProfileFields } from "@/Components/ProfileComponent";
+import { useState } from "react";
 
 const Profile = () => {
-    return (
+  const [isDisabled, setDisabled] = useState(true);
+  const [isClicked, setClicked] = useState(false);
+
+  const update = (event) => {
+    event.preventDefault();
+    // Get details from form
+    var { username, email, password } = document.forms[0];
+    // update DB logic
+    console.log(email.value);
+    console.log(username.value);
+    console.log(password.value);
+
+    // Set the buttons back to original state
+    setDisabled(true);
+    setClicked(false);
+  };
+
+  const Button = (props) => {
+    if (!props.isClicked) {
+      return (
+        <button
+          onClick={() => {
+            setDisabled(false);
+            setClicked(true);
+          }}
+        >
+          Update personal particulars
+        </button>
+      );
+    } else {
+      return (
         <div>
-            profile
+          <button type="submit">Submit</button>
+          <button
+            onClick={() => {
+              setDisabled(true);
+              setClicked(false);
+            }}
+          >
+            Cancel
+          </button>
         </div>
-    );
-}
+      );
+    }
+  };
+  return (
+    <div>
+      <h1>Username</h1>
+      <form onSubmit={update}>
+        <ProfileFields
+          label="Username: "
+          type="text"
+          name="username"
+          placeholder="Get from db"
+          disabled={isDisabled}
+        />
+        <ProfileFields
+          label="Email: "
+          type="email"
+          name="email"
+          placeholder="Get from db"
+          disabled={isDisabled}
+        />
+        <ProfileFields
+          label="Password: "
+          type="text"
+          name="password"
+          placeholder="Get from db"
+          disabled={isDisabled}
+        />
+        <Button isClicked={isClicked} />
+      </form>
+    </div>
+  );
+};
 
 export default Profile;
