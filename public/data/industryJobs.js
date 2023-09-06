384a1e13048d1cd3afd671cb375f6b70859f2f01
Alex Tran <alextran8385@gmail.com>
2023-09-06 21:43:57 +1000

    added images to dashboard

--- public/data/industryJobs.js
+++ public/data/industryJobs.js
@@ -1,9 +1,20 @@
+import IT from '../data/images/informationtechnologyimg.png'
+import Hospitality from '../data/images/hospitalityimg.png'
+import Business from '../data/images/businessimg.png'
+import Healthcare from '../data/images/healthcareimg.png'
+import Goverment from '../data/images/govermentimg.png'
+import Trade from '../data/images/tradeimg.png'
+import Education from '../data/images/educationimg.png'
+import Construction from '../data/images/constructionimg.png'
+import Engineering from '../data/images/engineeringimg.png'
+
 const industries = 
 [
     
     {
-        IndustryName: "Information Technology",
-        jobs: ["Software Developer", "Project Manager", "IT Support", "Cyber Security Officer", "IT Manager", "Software Intern",  "Cloud Architect", "Data Scientist"]
+        IndustryName: "IT",
+        jobs: ["Software Developer", "Project Manager", "IT Support", "Cyber Security Officer", "IT Manager", "Software Intern",  "Cloud Architect", "Data Scientist"],
+        image:{IT}
            
     }, 
 
@@ -11,44 +22,52 @@ const industries =
 
     {
         IndustryName: "Hospitality",
-        jobs: []
+        jobs: [],
+        image:{Hospitality}
     }, 
 
     {
         IndustryName: "Business",
-        jobs: []
+        jobs: [],
+        image:{Business}
     }, 
 
     {
         IndustryName: "Healthcare",
-        jobs: [] 
+        jobs: [],
+        image: {Healthcare}
     },
 
     {
         IndustryName: "Goverment",
-        jobs: []
+        jobs: [],
+        image:{Goverment}
     },
     
     {
         IndustryName: "Education",
-        jobs: [] 
+        jobs: [],
+        image: {Education}
     },
     
     {
         IndustryName: "Trade",
-        jobs: []
+        jobs: [],
+        image:{Trade}
         
     },
 
     {
         IndustryName: "Construction",
-        jobs: []
+        jobs: [],
+        image:{Construction}
         
     },
 
     {
         IndustryName: "Engineering",
-        jobs: []
+        jobs: [],
+        image:{Engineering}
         
     }
      
