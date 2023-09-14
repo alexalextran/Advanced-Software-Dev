75e0fc3097b937d274c759b8a278f98c4d214ae8
Alex Tran <alextran8385@gmail.com>
2023-09-14 23:18:13 +1000

    basic speec to text

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.scss";
 import "../styles/edward.css";
 import '../styles/history.css';
+import 'regenerator-runtime/runtime'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
