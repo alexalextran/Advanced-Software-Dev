7df652a7fed51f3bcaf0187a9d59b84514aee843
honeybuttercrisp <vietdragon.jv@gmail.com>
2023-09-15 03:17:10 +1000

    big changes

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 const Chat = () => {
+
+
+    fetch("https://tomsoderlund-rest-api-with-gradio.hf.space/run/predict", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    data: [
+      "hello world!!!",
+	]
+  })})
+.then(r => r.json())
+.then(
+  r => {
+    let data = r.data;
+    console.log(data)
+  }
+)
+
     return (
         <div>
             hello world!
