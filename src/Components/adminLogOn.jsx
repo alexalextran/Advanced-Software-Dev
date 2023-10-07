1d34093630537cdf68203d33d39062820483eb65
Alex Tran <alextran8385@gmail.com>
2023-10-07 22:29:48 +1100

    admin login component

---
+++ src/Components/adminLogOn.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import 'firebase/firestore';
+import {
+  collection,
+  getFirestore,
+  getDocs,
+} from "firebase/firestore"; 
+
+function AdminLogOn({db, setadminLoggedIn}) {
+    const [email, setemail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [admins, setadmins] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const adminsRef = collection(db, 'admins');
+            const snapshot = await getDocs(adminsRef);
+            const adminData = snapshot.docs.map((doc) => ({
+              ID: doc.id,
+              ...doc.data(),
+            }));
+            setadmins(adminData);
+          } catch (error) {
+            console.error('Error fetching industry jobs:', error);
+          }
+        };
+    
+        fetchData();
+      }, [db]);
+
+
+
+
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    // Check if there's an admin with the provided email and password
+    const adminMatch = admins.find((admin) => admin.email === email && admin.password === password);
+  
+    if (adminMatch) {
+      // Admin found, you can set adminLoggedIn to true here or perform any other action
+      console.log('Admin logged in');
+      setadminLoggedIn(true)
+    } else {
+      setError('Invalid credentials. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Admin Login</h2>
+      {error && <p className="error">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">email:</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AdminLogOn;
--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -9,6 +9,7 @@ import AdminIndustry from "@/Components/adminIndustry";
 import AdminJobDropDown from "@/Components/adminJobDropDown";
 import AdminAddQuestion from "@/Components/adminAddQuestion";
 import AdminAddJob from "@/Components/adminAddJob";
+import AdminLogOn from "@/Components/adminLogOn";
 
 const Admin = () => {
   const [industryArray, setIndustryArray] = useState([]); //all industries e.g --> IT, Hospitality, Finance etc
@@ -21,6 +22,9 @@ const Admin = () => {
   const [questions, setQuestions] = useState([]); // State for questions
 
   const db = getFirestore();
+
+  const [adminLoggedIn, setadminLoggedIn] = useState(false)
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -48,38 +52,50 @@ const Admin = () => {
 
   return (
     <div>
-  
-      {
-        <AdminIndustry industryArray={industryArray} db={db} setJobsArray={setJobsArray} selectedIndustry={selectedIndustry} setSelectedIndustry={setSelectedIndustry}/>
-      }
-
-      {selectedIndustry 
-      && 
-      (<AdminJobDropDown selectedJob={selectedJob} setSelectedJob={setSelectedJob} jobsArray={jobsArray}/>)
-        }
-
-      {selectedIndustry && selectedJob && (
-        <AdminAddQuestion question={question} jobsArray={jobsArray} selectedJob={selectedJob} db={db} selectedIndustry={selectedIndustry} questions={questions} setQuestion={setQuestion} setQuestions={setQuestions} setJobsArray={setJobsArray}/>
-      )}
-
-      {/* Render the added questions */}
-      {/* {selectedJob && questions.length > 0 && (
-        <div>
-          <h2>Added Questions:</h2>
-          <ul>
-            {questions.map((q, index) => (
-              <li key={index}>{q}</li>
-            ))}
-          </ul>
-        </div>
-      )} */}
-
-      {/* Form to add a new job */}
-      {selectedIndustry && (
-       <AdminAddJob newJob={newJob} setNewJob={setNewJob} setJobsArray={setJobsArray} jobsArray={jobsArray} db={db} selectedIndustry={selectedIndustry}/>
+      {!adminLoggedIn ? (
+        <AdminLogOn db={db} setadminLoggedIn={setadminLoggedIn}/>
+      ) : (
+        <>
+          <AdminIndustry
+            industryArray={industryArray}
+            db={db}
+            setJobsArray={setJobsArray}
+            selectedIndustry={selectedIndustry}
+            setSelectedIndustry={setSelectedIndustry}
+          />
+          {selectedIndustry && (
+            <AdminJobDropDown
+              selectedJob={selectedJob}
+              setSelectedJob={setSelectedJob}
+              jobsArray={jobsArray}
+            />
+          )}
+          {selectedIndustry && selectedJob && (
+            <AdminAddQuestion
+              question={question}
+              jobsArray={jobsArray}
+              selectedJob={selectedJob}
+              db={db}
+              selectedIndustry={selectedIndustry}
+              questions={questions}
+              setQuestion={setQuestion}
+              setQuestions={setQuestions}
+              setJobsArray={setJobsArray}
+            />
+          )}
+          {selectedIndustry && (
+            <AdminAddJob
+              newJob={newJob}
+              setNewJob={setNewJob}
+              setJobsArray={setJobsArray}
+              jobsArray={jobsArray}
+              db={db}
+              selectedIndustry={selectedIndustry}
+            />
+          )}
+        </>
       )}
     </div>
-  );
-};
+  );}
 
 export default Admin;
