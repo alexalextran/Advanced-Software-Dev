a7dcda836453c1e1a1594f64c5b38651afc2f9d8
eddyboon <66462284+eddyboon@users.noreply.github.com>
2023-10-07 17:15:33 +1100

    made user db during signup

--- src/pages/signupPage.js
+++ src/pages/signupPage.js
@@ -3,14 +3,14 @@ import { MdEmail } from "react-icons/md";
 import { FaLock, FaUser } from "react-icons/fa6";
 import { IconContext } from "react-icons";
 import LoginField from "@/Components/LoginComponent";
-import { useAuth } from '../../context/AuthContext'
+import { useAuth } from "../../context/AuthContext";
 import React, { useEffect } from "react";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 // use router.push('/Dashboard') to go redirect user to dashboard after user auth
 
 const SignupPage = () => {
   const router = useRouter();
-  const { user, signup } = useAuth()
-
+  const { user, signup } = useAuth();
 
   const signupfunc = async (event) => {
     event.preventDefault();
@@ -20,23 +20,28 @@ const SignupPage = () => {
     // use email.value to get value of variable
 
     try {
-      await signup(email.value, password.value)
+      await signup(email.value, password.value);
+
+      const db = getFirestore();
+      await setDoc(doc(db, "users", email.value), {
+        username: username.value,
+        password: password.value,
+      });
       router.replace("/Dashboard");
     } catch (err) {
-      alert(err.message)
-      console.log(err)
+      alert(err.message);
+      console.log(err);
     }
-    
 
     // Redirect to dashboard if valid
     // Todo: pass email to dashboard page
- 
+
     // Redirect if not valid
   };
 
   useEffect(() => {
-    if(user) router.push('/Dashboard')
-  }, [user])
+    if (user) router.push("/Dashboard");
+  }, [user]);
 
   return (
     <div className="formDiv">
