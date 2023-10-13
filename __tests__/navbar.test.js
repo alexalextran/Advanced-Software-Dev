9d86aa4b6fb76b9ee3aca65e405cd5a330a9297d
eddyboon <66462284+eddyboon@users.noreply.github.com>
2023-10-13 13:34:43 +1100

    Add navbar test

---
+++ __tests__/navbar.test.js
@@ -0,0 +1,37 @@
+import "@testing-library/jest-dom";
+import React, { useState as useStateMock } from "react";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  act,
+} from "@testing-library/react";
+import ChatHistory from "@/pages/history";
+import mockRouter from "next-router-mock";
+import { AuthContextProvider } from "../context/AuthContext";
+import Navigation from "@/pages/navigation";
+
+jest.mock("next/router", () => jest.requireActual("next-router-mock"));
+
+const TestNavbar = () => {
+  return <Navigation />;
+};
+
+describe("Render Navbar", () => {
+  test("Correctly render navbar", () => {
+    mockRouter.push("/Dashboard");
+    render(<TestNavbar />);
+    const dashboardLink = screen.getByText("Dashboard");
+    const analyticsLink = screen.getByText("Analytics");
+    const historyLink = screen.getByText("History");
+    const profileLink = screen.getByText("Profile");
+    const adminLink = screen.getByText("Admin");
+
+    expect(dashboardLink).toBeInTheDocument();
+    expect(analyticsLink).toBeInTheDocument();
+    expect(historyLink).toBeInTheDocument();
+    expect(profileLink).toBeInTheDocument();
+    expect(adminLink).toBeInTheDocument();
+  });
+});
