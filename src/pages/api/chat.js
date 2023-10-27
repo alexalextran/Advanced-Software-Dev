d271a19eaf7b81ff9f0d96b538f74c06534a2adb
Alex Tran <alextran8385@gmail.com>
2023-10-28 00:02:35 +1100

    a

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+//
 export default async function handler(req, res) {
   const referer = req.headers.referer || req.headers.referrer; // get the referer from the request headers
 
