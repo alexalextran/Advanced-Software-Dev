96b30e8a367b48feef884699547602270a36b8e5
Alex Tran <alextran8385@gmail.com>
2023-11-03 01:55:49 +1100

    potenetial fix

--- public/api/chat.js
+++ public/api/chat.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ message: 'Method should be POST' });
   } else if (process.env.NODE_ENV !== "development") {
-    if (!referer || referer !== `https://${VERCEL_URL}`) {
+    if (!referer || referer !== `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`) {
       res.status(401).json({ message: 'Unauthorized' });
     }
   }
