fbc6c988aba12a017e6a611b1a66f81e8042136e
Alex Tran <alextran8385@gmail.com>
2023-11-03 01:08:44 +1100

    new fix?

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method should be POST" });
   } else if (process.env.NODE_ENV !== "development") {
-    if (!referer || referer !== 'https://asd-fb4nxuehc-alexalextran.vercel.app/openai') {
+    if (!referer || referer !== `https://${VERCEL_URL}`) {
       res.status(401).json({ message: 'Unauthorized' });
     }
   } else {
