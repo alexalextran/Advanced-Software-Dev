23b86b60e942cce19136e0b797bba6a4dd21e26e
Alex Tran <alextran8385@gmail.com>
2023-11-03 02:20:58 +1100

    cors fix

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,27 +1,35 @@
-import axios from "axios";
+import axios from 'axios';
+import Cors from 'cors';
+import initMiddleware from '@/lib/init-middleware';
+
+const cors = initMiddleware(
+    Cors({
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Define the HTTP methods your API should support
+      origin: `https://${VERCEL_URL}`, // Replace with your frontend's domain
+    })
+  );
 
 export default async function handler(req, res) {
   const referer = req.headers.referer || req.headers.referrer; // get the referer from the request headers
 
-  if (req.method !== "POST") {
-    res.status(405).json({ message: "Method should be POST" });
+  if (req.method !== 'POST') {
+    res.status(405).json({ message: 'Method should be POST' });
   } else if (process.env.NODE_ENV !== "development") {
     if (!referer || referer !== `https://${VERCEL_URL}`) {
       res.status(401).json({ message: 'Unauthorized' });
     }
-  } else {
+  }
+  else {
     try {
+    await cors(req, res);
       const { body } = req;
-      const url = "https://api.openai.com/v1/chat/completions";
+      const url = 'https://api.openai.com/v1/chat/completions';
       const headers = {
-        "Content-type": "application/json",
-        Authorization: `Bearer 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`,
+        'Content-type': 'application/json',
+        'Authorization':`Bearer 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`
       };
 
-      const response = await axios.post(url, body, {
-        headers: headers,
-        timeout: 180000,
-      });
+      const response = await axios.post(url, body, { headers: headers })
 
       res.status(200).json(response.data);
     } catch (error) {
@@ -29,4 +37,5 @@ export default async function handler(req, res) {
       res.status(500).json({ message: "Something went wrong" });
     }
   }
+  
 }
