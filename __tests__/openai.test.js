418ec8c6863efd1479d2b6aed5dc510c3913358f
Alex Tran <alextran8385@gmail.com>
2023-11-03 01:26:24 +1100

    test removal

--- __tests__/openai.test.js
+++ __tests__/openai.test.js
@@ -22,23 +22,7 @@ jest.mock('../context/AuthContext', () => ({
     }),
 }));
 
-test('sends percentages to the database', async () => {
-    const { getByPlaceholderText, getByText } = render(<OpenAI />);
-    const inputField = getByPlaceholderText('Enter Response Here');
-    fireEvent.change(inputField, { target: { value: 'Sample response' } });
-    const sendButton = getByText('Send');
-    fireEvent.click(sendButton);
-    //Mock percentage from the AI Feedback response
-    const mockPercentages = {
-        Confidence: 50,
-        Coherence: 60,
-        Professionalism: 70,
-        Creativity: 80,
-    };
-    const addAanalyticsDBMock = jest.fn();
-    addAanalyticsDBMock(mockPercentages);
-    expect(addAanalyticsDBMock).toHaveBeenCalledWith(mockPercentages);
-});
+
 
 test("send button is disabled when there is no text in the textfield", () => {
     render(<OpenAI />);
