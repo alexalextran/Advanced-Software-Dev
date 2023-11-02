496211499940f192f8638c7de92097b0565b17b6
Joshua_Nguyen <joshua.nguyen07@gmail.com>
2023-11-02 19:18:05 +1100

    Added the feature that shows a message if the current user signed in does not have any previous chat conversations

--- src/pages/history.js
+++ src/pages/history.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navigation from './navigation';
-import { useAuth } from '../../context/AuthContext'
+import { useAuth } from '../../context/AuthContext';
 import Link from 'next/link';
 
 const ChatHistory = () => {
@@ -24,15 +24,15 @@ const ChatHistory = () => {
     setSelectedChat(chat);
   };
 
-  const analyticsButton =  {
+  const analyticsButton = {
     display: 'inline-block',
-    backgroundColor: 'black', 
+    backgroundColor: 'black',
     color: 'white',
     padding: '10px 25px',
     border: '1px solid grey',
     borderRadius: '5px',
     textDecoration: 'none',
-  }
+  };
 
   return (
     <>
@@ -40,37 +40,40 @@ const ChatHistory = () => {
       <div className="chat-history">
         <div className="chat-list">
           <h2>Chat History</h2>
-          <ul>
-            {history.map((chat) => (
-              <li
-                key={chat.id}
-                onClick={() => handleChatSelect(chat)}
-              >
-                {chat.ID}
-              </li>
-            ))}
-          </ul>
+          {history.length > 0 ? (
+            <ul>
+              {history.map((chat) => (
+                <li key={chat.id} onClick={() => handleChatSelect(chat)}>
+                  {chat.ID}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Nothing here to see! Go to <Link href="/dashboard">Dashboard</Link> to get started on some practice chats!</p>
+          )}
         </div>
         <div className="chat-display">
           <h2>Chat Display</h2>
           {selectedChat && (
             <div>
               <h3>{selectedChat.ID}</h3>
-              <p> <b>Interview Question</b></p>
-              <ul>
-                {selectedChat.InterviewQuestion}
-              </ul>
-              <p> <b>User Response</b></p>
-              <ul>
-                {selectedChat.userResponse}
-              </ul>
-              <p> <b>AI Response</b></p>
-              <ul>
-                {selectedChat.GPTResponse}
-              </ul>
-              <div style = {{textAlign: 'center', marginTop: '80px', marginBottom: '70px'}}>
-                <Link  href={"/analytics"} style={analyticsButton}>Check Analytics</Link>
-              </div>            
+              <p>
+                <b>Interview Question</b>
+              </p>
+              <ul>{selectedChat.InterviewQuestion}</ul>
+              <p>
+                <b>User Response</b>
+              </p>
+              <ul>{selectedChat.userResponse}</ul>
+              <p>
+                <b>AI Response</b>
+              </p>
+              <ul>{selectedChat.GPTResponse}</ul>
+              <div style={{ textAlign: 'center', marginTop: '80px', marginBottom: '70px' }}>
+                <Link href="/analytics" style={analyticsButton}>
+                  Check Analytics
+                </Link>
+              </div>
             </div>
           )}
         </div>
