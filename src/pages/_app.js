79656741c6bb8b2e6ec36fa5f581d05ddedda0ec
eddyboon <66462284+eddyboon@users.noreply.github.com>
2023-11-03 13:12:00 +1100

    remove weird colon rendering on page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,22 @@
 import "@/styles/globals.scss";
 import "../styles/edward.css";
-import '../styles/history.css';
+import "../styles/history.css";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
-import { AuthContextProvider  } from '../../context/AuthContext';
-import { useAuth } from '../../context/AuthContext'
+import { AuthContextProvider } from "../../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 export default function App({ Component, pageProps }) {
-  const { user } = useAuth()
+  const { user } = useAuth();
   const router = useRouter();
   useEffect(() => {
-
-    if (!user && router.pathname !== '/signupPage') {
-      router.push('/');
+    if (!user && router.pathname !== "/signupPage") {
+      router.push("/");
     }
+  }, [user]);
 
-  }, [user])
-
-
-
-  return <AuthContextProvider><Component {...pageProps} />;</AuthContextProvider>
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
